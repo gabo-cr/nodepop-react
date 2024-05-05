5c771ad8d3161545d84a487e30d7248a02c7fee4
Gabriel Chávez Riera <gabrielchavezriera@gmail.com>
2024-05-04 21:32:01 -0600

    Add modal to logout button.

--- src/components/auth/AuthButton.tsx
+++ src/components/auth/AuthButton.tsx
@@ -1,17 +1,38 @@
 import { useAuth } from '../../context/AuthContextProvider';
 import { logout } from '../../api/auth';
 import Button from '../shared/button/Button';
+import Modal from '../shared/modal/Modal';
+import { useState } from 'react';
 
 export default function AuthButton() {
 	const { isLogged, onLogout } = useAuth();
+
+	const [openModal, setOpenModal] = useState<boolean>(false);
 
 	const handleLogoutClick = async () => {
 		await logout();
 		onLogout();
 	};
 
+	const handleModalCancel = () => {
+		setOpenModal(false);
+	};
+
+	const handleModalContinue = () => {
+		handleLogoutClick();
+		setOpenModal(false);
+	};
+
 	return isLogged ? (
-		<Button onClick={handleLogoutClick} variant='secondary'>Cerrar sesión</Button>
+		<>
+			<Button onClick={() => setOpenModal(true)} variant='secondary'>Cerrar sesión</Button>
+			<Modal 
+				open={openModal}
+				message="¿Estás seguro de querer cerrar sesión?"
+				onCancel={handleModalCancel}
+				onContinue={handleModalContinue}
+			/>
+		</>
 	) : (
 		<Button variant='link' to='/login'>Iniciar sesión</Button>
 	);
