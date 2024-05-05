0d4d0b1abac3abf9a05fd4461ed4624f8f525b9a
Gabriel Chávez Riera <gabrielchavezriera@gmail.com>
2024-05-04 20:50:34 -0600

    Add loader to the adverts list page.

--- src/pages/adverts/AdvertsPage.tsx
+++ src/pages/adverts/AdvertsPage.tsx
@@ -9,31 +9,46 @@ import Advert from "../../components/adverts/Advert";
 import EmptyList from "../../components/adverts/EmptyList";
 import './AdvertsPage.css';
 import DashboardLayout from "../../components/layout/DashboardLayout";
+import Loader from "../../components/shared/loader/Loader";
 
 export default function AdvertsPage() {
 	const [adverts, setAdverts] = useState<TAdvert[]>([]);
 	const [error, setError] = useState<TResponseError | null>(null);
+	const [isLoading, setIsLoading] = useState<boolean>(true);
 
 	useEffect(() => {
 		const getAllAdverts = async () => {
 			try {
+				setIsLoading(true);
 				const allAdverts = await getAdverts();
 				setAdverts(allAdverts);
+				setIsLoading(false);
 			} catch (error: any) {
 				setError(error);
+				setIsLoading(false);
 			}
 		}
 
 		getAllAdverts();
 	}, []);
 
+	if (isLoading) {
+		return (
+			<DashboardLayout title="Anuncios">
+				<Loader />
+			</DashboardLayout>
+		);
+	}
+
 	return (
 		<DashboardLayout title="Anuncios">
-		{error && (
-			<Alert variant="error">
-				{error.message}
-			</Alert>
-		)}
+		{
+			error && (
+				<Alert variant="error">
+					{error.message}
+				</Alert>
+			)
+		}
 		{
 			adverts.length ? (
 				<ul className="advertsList">
